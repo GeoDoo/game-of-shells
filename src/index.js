c69f6e2889135ca19e1832a7688197a994a4c5ba
George Karametas <georgios.karametas@ideato.info>
2018-10-03 22:06:29 +0200

    Setup React and babel and webpack html plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('test')
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+  return <div>Hello React!</div>
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
