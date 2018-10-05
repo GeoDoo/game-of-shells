5bc132cd6e23ece8379a493afbbf756e30bbe1c4
George Karametas <georgios.karametas@ideato.info>
2018-10-05 12:04:30 +0200

    Configure jest puppeteer integration

--- __e2e__/App.int.test.js
+++ __e2e__/App.int.test.js
@@ -1,24 +1,5 @@
-import puppeteer from 'puppeteer'
-
-describe('Bare-bones test with puppeteer', () => {
-  let browser
-  let page
-  let originalTimeout
-
-  beforeAll(async () => {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 9000000
-
-    browser = await puppeteer.launch()
-    page = await browser.newPage()
-  })
-
-  afterAll(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout
-    browser.close()
-  })
-
-  it('should load the app', async () => {
+describe('App', () => {
+  it('As a user I can start the app', async () => {
     await page.goto('http://localhost:3000/')
   })
 })
