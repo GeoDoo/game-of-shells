1aefd27c5c71d3a4713c6c147ec340c894365527
George Karametas <georgios.karametas@ideato.info>
2018-10-06 17:23:40 +0200

    Add react prop types validation

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Ball = ({
   imagePath
 }) => <img testkey="ball" src={imagePath} />
+
+Ball.propTypes = {
+  imagePath: PropTypes.string.isRequired
+}
 
 export default Ball
--- src/components/Shell.js
+++ src/components/Shell.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Shell = ({
   imagePath,
   onClick
 }) => <img testkey="shell" src={imagePath} onClick={onClick} />
+
+Shell.propTypes = {
+  imagePath: PropTypes.string.isRequired
+}
 
 export default Shell
