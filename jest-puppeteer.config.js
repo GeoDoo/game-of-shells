6b4957bfa232fac7f26fecc539eb0fdf4b853981
George Karametas <georgios.karametas@ideato.info>
2018-10-19 13:59:31 +0200

    Disable headless false

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
-const isNotHeadlessOnTests = process.env.NODE_ENV !== 'test'
+const shouldBeFalseForTests = process.env.NODE_ENV !== 'test'
 
 module.exports = {
   launch: {
-    headless: isNotHeadlessOnTests,
+    headless: !shouldBeFalseForTests,
     slowMo: 500
   }
 }
